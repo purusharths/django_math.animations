db48ef7f5e71326315cf8a00acbff6ceab667eb6
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2019-11-26 08:58:04 +0530

    Static errors fixed

--- fossee_math_pages/urls.py
+++ fossee_math_pages/urls.py
@@ -5,12 +5,12 @@ from fossee_math_pages import views
 from django.urls import path
 
 urlpatterns = [
-    url(r'^login/$',views.login,name='login'),
-    path('',views.index, name='index'),
-    path('',views.index, name='pagenotfound'),
-    path('',views.index, name='content_emplate'),
-    path('',views.index, name='internship'),
-    path('',views.index, name='real_number_line'),
-    path('',views.index, name='realanalysis'),
-    path('',views.index, name='topics'),
+    url(r'^login/$', views.login, name='login'),
+    path(r'', views.index, name='index'),
+    path(r'', views.index, name='pagenotfound'),
+    path(r'', views.index, name='content_emplate'),
+    path(r'', views.index, name='internship'),
+    path(r'', views.index, name='real_number_line'),
+    path(r'', views.index, name='realanalysis'),
+    path('topics/', views.topics, name='topics'),
 ]
--- fossee_math_pages/views.py
+++ fossee_math_pages/views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render
-from .forms import (UserLoginForm,)
-from .models import (profile, User,)
+from .forms import (UserLoginForm, )
+from .models import (profile, User, )
 from os import listdir, path, sep, makedirs, remove
 from datetime import datetime, date
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 
+
 def index(request):
     return render(request, "fossee_math_pages/index.html")
+
 
 def pagenotfound(request):
     return render(request, "fossee_math_pages/404.html")
 
+
 def internship(request):
     return render(request, "fossee_math_pages/internship.html")
 
+
 def real_number_line(request):
-    return render(request, "fossee_math_pages/real_number_line).html")
+    return render(request, "fossee_math_pages/real_number_line.html")
+
 
 def realanalysis(request):
     return render(request, "fossee_math_pages/realanalysis.html")
 
+
 def topics(request):
     return render(request, "fossee_math_pages/topics.html")
 
+
 def is_superuser(user):
     return user.is_superuser
+
 
 def login(request):
     try:
