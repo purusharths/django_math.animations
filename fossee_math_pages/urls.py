7d2d48ffa034b71473c97adde5ad69a524f29490
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2019-11-29 13:59:26 +0530

    corrections

--- fossee_math_pages/urls.py
+++ fossee_math_pages/urls.py
@@ -11,11 +11,10 @@ urlpatterns = [
     path('logout/',views.user_logout,name='logout'),
     path('internship/', views.internship, name='internship'),
     path('topics/', views.topics, name='topics'),
+    path('topic_details/',views.topic_details,name='topic_details'),
     path('dashboard/',views.dashboard,name='dashboard'),
-
-    path('', views.index, name='real_number_line'),
-    path('', views.index, name='realanalysis'),
-    path('', views.index, name='pagenotfound'),
-    path('', views.index, name='content_emplate'),
-    # path('', views.InternForm),
+    path('manage_intern/',views.manage_intern,name='manage_intern'),
+    path('aprove_contents/',views.aprove_contents,name='aprove_contents'),
+    path('add_details/',views.add_details,name='add_details'),
+    path('view_details/',views.view_details,name='view_details'),
 ]
