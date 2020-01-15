9dfcc033523ed05fe7ea92de5a545e433c6287a2
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-01-15 09:56:29 +0530

    ckeditor implementation

--- FOSSEE_math/settings.py
+++ FOSSEE_math/settings.py
@@ -125,12 +125,8 @@ LOGOUT_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
