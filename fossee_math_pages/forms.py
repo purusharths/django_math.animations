a6ef68583d280fc3477d0c3878d9e5125fae6475
ShijuKAbraham <k.ashiju10@gmail.com>
2020-01-16 21:14:47 +0530

    aaaaaaaaaaaaaaaaaaaaaaa

--- fossee_math_pages/forms.py
+++ fossee_math_pages/forms.py
@@ -81,21 +81,38 @@ class data(ModelForm):
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(max_length=32, widget=forms.TextInput())
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput())
+    email = forms.CharField()
+    password = forms.CharField(
+        widget=forms.PasswordInput(render_value=False)
+        )
 
     def clean(self):
-        super(UserLoginForm, self).clean()
-        try:
-            uname, pwd = self.cleaned_data["username"], \
-                         self.cleaned_data["password"]
-            user = authenticate(username=uname, password=pwd)
-        except Exception:
-            raise forms.ValidationError \
-                ("Username and/or Password is not entered")
+        user = self.authenticate_via_email()
         if not user:
-            raise forms.ValidationError("Invalid username/password")
-        return user
+            raise forms.ValidationError("Sorry, that login was invalid. Please try again.")
+        else:
+            self.user = user
+        return self.cleaned_data
+
+    def authenticate_user(self):
+        return authenticate(
+            username=self.user.username,
+            password=self.cleaned_data['password'])
+
+    def authenticate_via_email(self):
+        """
+            Authenticate user using email.
+            Returns user object if authenticated else None
+        """
+        email = self.cleaned_data['email']
+        if email:
+            try:
+                user = User.objects.get(email__iexact=email)
+                if user.check_password(self.cleaned_data['password']):
+                    return user
+            except ObjectDoesNotExist:
+                pass
+        return None
 
 
 class add_topic(forms.Form):
