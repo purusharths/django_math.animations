13cd41c86164dd75af4ccd46d6aef38c30836318
ShijuKAbraham <k.ashiju10@gmail.com>
2020-01-16 20:33:39 +0530

    aaaaaaaaaaaaaaaaaaaaaaa

--- fossee_math_pages/views.py
+++ fossee_math_pages/views.py
@@ -380,6 +380,10 @@ def admin_view_users(request):
     user_contains_query = request.GET.get('title_contains')
     if user_contains_query !='' and user_contains_query is not None:
         datas = UserDetails.objects.filter(user_id__username__contains=user_contains_query)
+    if user_contains_query == "STAFF" or user_contains_query == "staff":
+        datas = UserDetails.objects.filter(user_role="STAFF")
+    if user_contains_query == "INTERN" or user_contains_query == "intern":
+        datas = UserDetails.objects.filter(user_role="INTERN")
     context = {
         'datas' : datas,  
     }
