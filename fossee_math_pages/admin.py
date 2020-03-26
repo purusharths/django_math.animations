9577aac1eacee2ce623d19205d55829a9caa5c32
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-03-26 22:35:22 +0530

    Adding Topic , Subtopic Tables to Admin Console

--- fossee_math_pages/admin.py
+++ fossee_math_pages/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import (UserDetails, Internship, Intern, AssignedTopics, Data)
+from .models import (UserDetails, Internship, Intern, AssignedTopics, Data, Topic, Subtopic, Contributor)
 
 # # Register your models here.
 # myModels = [AddUser,data]
 # admin.site.register(myModels)
 
-myModels = [UserDetails, Internship, Intern, AssignedTopics]
+myModels = [UserDetails, Internship, Intern, AssignedTopics, Topic, Subtopic, Contributor]
 admin.site.register(myModels)
 
 admin.site.register(Data)
