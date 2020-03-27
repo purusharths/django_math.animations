5bd0038bba6454d239d336b7e305fca926457a21
thedevboy <jayaspjacob@gmail.com>
2020-03-27 09:32:28 +0530

    ui changes for recent issues

--- fossee_math_pages/admin.py
+++ fossee_math_pages/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import (UserDetails, Internship, Intern, AssignedTopics, Data)
+from .models import (UserDetails, Internship, Intern, AssignedTopics, Data,Topic)
 
 # # Register your models here.
 # myModels = [AddUser,data]
 # admin.site.register(myModels)
 
-myModels = [UserDetails, Internship, Intern, AssignedTopics]
+myModels = [UserDetails, Internship, Intern, AssignedTopics,Topic]
 admin.site.register(myModels)
 
 admin.site.register(Data)
