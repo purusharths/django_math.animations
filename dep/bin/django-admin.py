98bbb1af597dcd8ebcb167202db8fe898f54d2db
Purusharth S <purusharth.saxena1@gmail.com>
2020-04-22 18:04:49 +0530

    remove venv folder

--- dep/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/anuj/delete_this/math_animate1/django_math.animations/dep/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
