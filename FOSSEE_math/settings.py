d1e0079c5542434600e390edad97db1fa0fcb10b
Ubuntu <ubuntu@ip-172-31-39-215.us-east-2.compute.internal>
2020-04-26 14:15:28 +0000

    bump pillow version in requirements.txt (error on ubnutu 20.04)

--- FOSSEE_math/settings.py
+++ FOSSEE_math/settings.py
@@ -127,7 +127,7 @@ LOGOUT_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'FOSSEE_math/static'),
