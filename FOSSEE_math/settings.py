de75e3ce24b52992f541d70f4a54162939823351
Alwin1847207 <alwin.joseph@mca.christuniversity.in>
2020-04-28 01:14:11 +0530

    password reset complete

--- FOSSEE_math/settings.py
+++ FOSSEE_math/settings.py
@@ -179,10 +179,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
