89d7cf7a868fe3927c119544180a6084d2cf607a
Sh-Ac <sharanya.achut@gmail.com>
2020-04-27 11:06:59 +0530

    Change urls and views related to edit-image

--- fossee_math_pages/urls.py
+++ fossee_math_pages/urls.py
@@ -10,17 +10,11 @@ urlpatterns = [
                   path('staff_delete_data/<str:id>', views.staff_delete_data, name='staff_delete_data'),
                   path('staff_aprove_subtopic/<int:id>', views.staff_aprove_subtopic, name='staff_aprove_subtopic'),
                   path('staff_reject_subtopic/<int:id>', views.staff_reject_subtopic, name='staff_reject_subtopic'),
-                  path('staff_update_image_size/<str:id>', views.staff_update_image_size,
-                       name='staff_update_image_size'),
-
                   path('intern_delete_data/<str:id>', views.intern_delete_data, name='intern_delete_data'),
-                  path('intern_update_image_size/<str:id>', views.intern_update_image_size,
-                       name='intern_update_image_size'),
                   path('intern_update_media/<str:id>', views.intern_update_media, name='intern_update_media'),
 
+
                   # CHANGED
-                  path('dashboard/review-submissons/<str:t_id>/edit-text/<str:id>', views.edit_text, name='edit-text-staff'),
-                  path('dashboard/add-submisson/<str:t_id>/edit-text/<str:id>', views.edit_text, name='edit-text'),
                   path('dashboard/messages/', views.view_messages, name='messages'),
                   path('dashboard/add-internship/', views.add_internship, name='add-internship'),
                   path('dashboard/add-users/', views.add_users, name='add-users'),
@@ -29,6 +23,8 @@ urlpatterns = [
                   path('dashboard/add-submission/', views.add_submission, name='add-submission'),
                   path('dashboard/add-submission/<str:st_id>', views.add_submission_subtopic,
                        name='add-submission-subtopic'),
+                  path('dashboard/add-submisson/<str:t_id>/edit-image/<str:id>', views.edit_image, name='edit-image'),
+                  path('dashboard/add-submisson/<str:t_id>/edit-text/<str:id>', views.edit_text, name='edit-text'),
                   path('dashboard/assign-topics', views.assign_topics, name='assign-topics'),
                   path('dashboard/interns/', views.interns, name='interns'),
                   path('dashboard/internship-progress/', views.internship_progress, name='internship-progress'),
@@ -37,6 +33,8 @@ urlpatterns = [
                   path('dashboard/review-submissions/', views.review_submissions, name='review-submissions'),
                   path('dashboard/review-submissions/<str:s_id>', views.review_submissions_subtopic,
                        name='review-submissions-subtopic'),
+                  path('dashboard/review-submissons/<str:t_id>/edit-image/<str:id>', views.edit_image, name='edit-image-staff'),
+                  path('dashboard/review-submissons/<str:t_id>/edit-text/<str:id>', views.edit_text, name='edit-text-staff'),
                   ##
 
                   # ALREADY OKAY
